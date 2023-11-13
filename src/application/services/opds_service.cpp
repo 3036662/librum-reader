7802ba25c9b31bfc58181e30c25c8d3f3264a3b4
Oleg Proskurin <proskur@altlinux.org>
2023-11-13 12:21:23 +0300

    load root nodes from json file

--- src/application/services/opds_service.cpp
+++ src/application/services/opds_service.cpp
@@ -1,14 +1,59 @@
 #include "opds_service.hpp"
 
-namespace application::services
-{
+namespace application::services {
 
-const std::vector <domain::value_objects::OpdsNode>& OpdsService::getOpdsNodes() {
-    m_opdsNodes.emplace_back("Flibusta");
-    m_opdsNodes.emplace_back("Coollib");
-    m_opdsNodes.emplace_back("Всенаука");
-    return m_opdsNodes;
+const std::vector<domain::value_objects::OpdsNode>&
+OpdsService::getOpdsNodes() {
+  loadRootNodesFromFile();
+  return m_opdsNodes;
 }
 
+void OpdsService::loadRootNodesFromFile() {
+  m_opdsNodes.clear();
+  QString localAppFolder =
+      QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+  QDir destDir(localAppFolder);
+  if (!destDir.exists()) destDir.mkpath(".");
+  QString filepath = destDir.filePath("opds.json");
+  QFile opdsFile(filepath);
+  if (!opdsFile.exists()) {
+    qDebug() << "Json file not found";
+    m_opdsNodes.clear();
+    return;
+  }
+  QJsonParseError jsonParseError;
+  QJsonDocument jsonDoc;
+  // open file
+  if (opdsFile.open(QIODevice::ReadOnly)) {
+    QByteArray bytesFromFile = opdsFile.readAll();
+    if (bytesFromFile.isEmpty()) {
+      qDebug() << "Opds libs json file is empty";
+      return;
+    }
+    jsonDoc = QJsonDocument::fromJson(bytesFromFile, &jsonParseError);
+    if (jsonDoc.isNull()) {
+      qDebug() << "Can't parse opds libs json file. "
+               << jsonParseError.errorString();
+      return;
+    }
+  } else {
+    qDebug() << "Can't open opds libs json file.";
+    return;
+  }
+  opdsFile.close();
+  const QJsonArray libsJsonArray = jsonDoc.array();
+  if (libsJsonArray.isEmpty()) {
+    qDebug() << "libs array is empty";
+    return;
+  }
+  // create nodes
+  for (auto it = libsJsonArray.cbegin(); it != libsJsonArray.cend(); ++it) {
+    QJsonObject lib(it->toObject());
+    if (lib.isEmpty()) continue;
+    m_opdsNodes.emplace_back(lib.value("title").toString(),
+                             lib.value("url").toString(),
+                             lib.value("descr").toString());
+  }
+}
 
-  } // namespace application::services
+}  // namespace application::services
--- src/application/services/opds_service.hpp
+++ src/application/services/opds_service.hpp
@@ -1,17 +1,22 @@
 #pragma once
+#include <QDebug>
+#include <QDir>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QStandardPaths>
+
 #include "i_opds_service.hpp"
 
-namespace application::services
-{
+namespace application::services {
 
+class APPLICATION_EXPORT OpdsService : public IOpdsService {
+ public:
+  const std::vector<domain::value_objects::OpdsNode>& getOpdsNodes() override;
 
-class APPLICATION_EXPORT OpdsService : public IOpdsService
-{
-public:
-    const std::vector <domain::value_objects::OpdsNode>& getOpdsNodes() override;
-
-private:
-    std::vector <domain::value_objects::OpdsNode> m_opdsNodes;
+ private:
+  void loadRootNodesFromFile();
+  std::vector<domain::value_objects::OpdsNode> m_opdsNodes;
 };
 
-} // namespace application::services
+}  // namespace application::services
