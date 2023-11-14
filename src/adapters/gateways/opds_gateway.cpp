bb6bc858f90b4a61acc9f932c6d4ba183c96b3c4
Oleg Proskurin <proskur@altlinux.org>
2023-11-14 15:56:44 +0300

    prepare vector of nodes

--- src/adapters/gateways/opds_gateway.cpp
+++ src/adapters/gateways/opds_gateway.cpp
@@ -1,6 +1,11 @@
 #include "opds_gateway.hpp"
+#include "opdsparser.hpp"
+
 
 namespace adapters::gateways{
+using namespace application::utility::opds ;
+using namespace  domain::value_objects;
+
 
 OpdsGateway::OpdsGateway(IOpdsAccess* opdsAccess)
     : m_OpdsAccess(opdsAccess){
@@ -15,7 +20,20 @@ void OpdsGateway::loadRootlib(QString url){
 }
 
 void OpdsGateway::parseOpdsResonse(const QByteArray& data){
+    OpdsParser parser(
+        std::string (data.constData(),data.length())
+        );
 
+    std::vector<OpdsNode> res;
+    for (auto it=parser.dom.entries.cbegin(); it!=parser.dom.entries.end(); ++it){
+        res.emplace_back(
+            it->title.c_str(),
+            parser.getEntryUrlByID(it->id).c_str(),
+            it->content.empty()  ?  "" : it->content[0].text.c_str(),
+            it->id.c_str()
+            );
+    }
+    emit parsingXmlDomCompleted(res);
 }
 
 } // namespace adapters::gateways
