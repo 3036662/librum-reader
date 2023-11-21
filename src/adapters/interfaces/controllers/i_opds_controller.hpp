73e5e21098f4009af24f4f9c1642bb552b5792ed
Oleg Proskurin <proskur@altlinux.org>
2023-11-21 18:07:41 +0300

    remove deleteImage method from interface

--- src/adapters/interfaces/controllers/i_opds_controller.hpp
+++ src/adapters/interfaces/controllers/i_opds_controller.hpp
@@ -12,6 +12,7 @@ public:
     virtual adapters::data_models::OpdsModel*  getOpdsModel() =0;
     Q_INVOKABLE  virtual  void loadRootLib(const QString& url)=0;
     Q_INVOKABLE virtual void getNodeImage(const QString& id)=0;
+    //Q_INVOKABLE virtual void deleteNodeImage(const QString& id)=0;
 signals:
 
 };
