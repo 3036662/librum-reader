c3cad209f3408df5ac164b47f740ec89fcf436b7
Oleg Proskurin <proskur@altlinux.org>
2023-11-23 18:47:59 +0300

    add constructor

--- src/domain/entities/opds_node.hpp
+++ src/domain/entities/opds_node.hpp
@@ -1,10 +1,31 @@
 #pragma once
 #include <QString>
 #include <QImage>
+#include <QVector>
 
 namespace domain::value_objects {
 
 struct OpdsNode {
+
+    OpdsNode() =default;
+    OpdsNode(const QString& _title,
+                        const QString& _author,
+                        const QString& _url,
+                        const QString& _descr,
+                        const QString& _id,
+                        const QString& _imageUrl,
+                        const QImage& _imageObj =QImage(),
+                        const bool _imgDataReady = false,
+             const QVector<QPair<QString,QString>> _downloadUrls =  QVector<QPair<QString,QString>>()
+             )
+        :title(_title),
+        author(_author),
+        url(_url), descr(_descr),
+        id{_id},imageUrl(_imageUrl),
+        imageObj(_imageObj),
+        imgDataReady(_imgDataReady),
+        downloadUrls(_downloadUrls){}
+
   QString title;
   QString author;
   QString url;
@@ -13,6 +34,7 @@ struct OpdsNode {
   QString imageUrl;
   QImage imageObj;
   bool imgDataReady;
+  QVector<QPair<QString,QString>> downloadUrls;
 };
 
 }  // namespace domain::value_objects
